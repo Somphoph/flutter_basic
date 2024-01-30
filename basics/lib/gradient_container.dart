634fdfea20a80923672e8d62da78b67e6d5ee4f6
Somphop Homprasert <somphop.homprasert@krungthai.com>
2024-01-30 21:41:03 +0700

    Reuseable Widget & Constructor function

--- basics/lib/gradient_container.dart
+++ basics/lib/gradient_container.dart
@@ -2,24 +2,22 @@ import 'package:basics/styled_text.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  final List<Color> colors;
+
+  const GradientContainer({super.key, required this.colors});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 26, 2, 80),
-          Color.fromARGB(255, 45, 7, 98),
-        ],
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colors,
             begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-        ),
+            end: Alignment.bottomRight),
       ),
       child: const Center(
         child: StyledText('Hello World'),
       ),
     );
   }
-
 }
--- basics/lib/main.dart
+++ basics/lib/main.dart
@@ -1,14 +1,13 @@
-
 import 'package:basics/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
-      body: GradientContainer()
-      ,)
-    ,)
-  );
+      body: GradientContainer(colors:[
+        Color.fromARGB(255, 26, 2, 80),
+        Color.fromARGB(255, 45, 7, 98),
+      ]),
+    ),
+  ));
 }
-
-
