a4ac88014f4f90b03f50694f88a2eb0bdd661c91
Somphop Homprasert <somphop.homprasert@krungthai.com>
2024-02-05 22:15:32 +0700

    Update splash screen style

--- adv_basics/lib/splash_screen.dart
+++ adv_basics/lib/splash_screen.dart
@@ -5,46 +5,36 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Colors.purple[900]!, Colors.purple[800]!],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24.0,
             ),
-            const SizedBox(
-              height: 60,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
             ),
-            const Text(
-              'Learn Flutter the fun way!',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-              ),
-              child: const Text('Start Quiz'),
-            ),
-          ],
-        ),
+            child: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
-
 }
