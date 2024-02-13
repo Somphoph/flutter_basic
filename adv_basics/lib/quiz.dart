7af893c88bfc6454a48c69e9c59044ea3ec57b5f
Somphop Homprasert <somphop.homprasert@krungthai.com>
2024-02-13 22:08:50 +0700

    Add Result Screen

--- adv_basics/lib/quiz.dart
+++ adv_basics/lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:adv_basics/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:adv_basics/data/questions.dart';
@@ -28,7 +29,7 @@ class _QuizState extends State<Quiz> {
     if (selectedAnswers.length == questions.length) {
       selectedAnswers = [];
       setState(() {
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
@@ -42,6 +43,11 @@ class _QuizState extends State<Quiz> {
         onSelectAnswer: chooseAnswer,
       );
     }
+
+    if(activeScreen=='results-screen'){
+      screenWidget=const ResultsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
